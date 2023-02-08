154631228a1942bc2f290bcbbeb45c613c654659
khizerkamal <khizerkamal.03@gmail.com>
2023-02-08 18:22:30 +0500

    chore:update docker config

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -54,7 +54,7 @@ export const registerUserHandler = async (
         user,
       },
     });
-  } catch (err: any) {
+  } catch (err:any) {
     if (err.code === '23505') {
       return res.status(409).json({
         status: 'fail',
@@ -120,38 +120,37 @@ export const refreshAccessTokenHandler = async (
       );
       
       if (!decoded) {
-      console.log(decoded)
-      return next(new AppError(403, message));
+        return next(new AppError(403, message));
+      }
+
+      // Check if user still exist
+      const user = await findUserById(decoded.sub);
+
+      if (!user) {
+        return next(new AppError(403, message));
+      }
+
+      // Sign new access token7
+      const access_token = signJwt({ sub: user.id }, 'accessTokenPrivateKey', {
+        expiresIn: `${config.get<number>('accessTokenExpiresIn')}m`,
+      });
+
+      // 4. Add Cookies
+      res.cookie('access_token', access_token, accessTokenCookieOptions);
+      res.cookie('logged_in', true, {
+        ...accessTokenCookieOptions,
+        httpOnly: false,
+      });
+
+      // 5. Send response
+      res.status(200).json({
+        status: 'success',
+        access_token,
+      });
+    } catch (err: any) {
+      next(err);
     }
-
-    // Check if user still exist
-    const user = await findUserById(decoded.sub);
-
-    if (!user) {
-      return next(new AppError(403, message));
-    }
-
-    // Sign new access token
-    const access_token = signJwt({ sub: user.id }, 'accessTokenPrivateKey', {
-      expiresIn: `${config.get<number>('accessTokenExpiresIn')}m`,
-    });
-
-    // 4. Add Cookies
-    res.cookie('access_token', access_token, accessTokenCookieOptions);
-    res.cookie('logged_in', true, {
-      ...accessTokenCookieOptions,
-      httpOnly: false,
-    });
-
-    // 5. Send response
-    res.status(200).json({
-      status: 'success',
-      access_token,
-    });
-  } catch (err: any) {
-    next(err);
-  }
-};
+  };
 
 const logout = (res: Response) => {
   res.cookie('access_token', '', { maxAge: 1 });
@@ -167,7 +166,6 @@ export const logoutHandler = async (
   try {
     const user = res.locals.user;
 
-    // await redisClient.del(user.id);
     logout(res);
 
     res.status(200).json({
